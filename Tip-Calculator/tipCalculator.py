829b18489f6d3fc6471c09865d9afecb02a52e7c
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-02-04 00:19:52 +0800

    Updated Global Total

--- Tip-Calculator/tipCalculator.py
+++ Tip-Calculator/tipCalculator.py
@@ -3,21 +3,26 @@ print("Welcome to the tip calculator.")
 
 totalBill = input("What was the total bill? $")
 totalPeople = input("How many people to split the bill? ")
-percentChoice = input("What percentage tip would you like to give? 10, 12, or 15? ")
+percentChoice = input(
+    "What percentage tip would you like to give? 10, 12, or 15? ")
+
+total = 0
+
+
+def allInAll():
+    global total
+    total += float(totalBill)
+    total /= float(totalPeople)
 
 
 if percentChoice == "12":
-   total = float(totalBill) * 0.12
-   total = total + float(totalBill)
-   total = total / float(totalPeople)
+    total = float(totalBill) * 0.12
+    allInAll()
 elif percentChoice == "10":
-   total = float(totalBill) * 0.10
-   total = total + float(totalBill)
-   total = total / float(totalPeople)
+    total = float(totalBill) * 0.10
+    allInAll()
 elif percentChoice == "15":
-   total = float(totalBill) * 0.15
-   total = total + float(totalBill)
-   total = total / float(totalPeople)
+    total = float(totalBill) * 0.15
+    allInAll()
 
 print("Each person should pay: ${:.2f}".format(total))
-
