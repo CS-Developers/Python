e7892717c61660ff4eef32addfa89ef289aaf7f2
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-02-03 20:53:09 +0800

    Relearning the Basic Concatenation format using Python

--- Band-Name-Generator/main.py
+++ Band-Name-Generator/main.py
@@ -1,4 +1,10 @@
+# Basic print / showing output using print
 print("Welcome to the Band Name Generator.")
+
+# User input
+# It also can be cityName = input() / petName = input()
 cityName = input("What's name of the city you grew up in? ")
 petName = input("What's your pet's name? ")
+
+# Printing the output using the formatting function
 print("Your band name could be {} {}".format(cityName, petName))
